09d8a76905a02faac552ed4041a52722e5b1593c
MaryamMaksour <118085088+MaryamMaksour@users.noreply.github.com>
2023-08-13 00:00:01 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, jsonify
 from app import ask
+from flask_ngrok import run_with_ngrok
 
+app_n = Flask(__name__)
+run_with_ngrok(app_n)
 
-app = Flask(__name__)
-
-
-@app.route('/algo', methods=['GET'])
+@app_n.route('/', methods=['GET'])
 def api():
     d = {}
-    data = str(request.args['query'])
+    data = str(request.args['message'])
     answer = str(ask(data))
     d['answer'] = answer
     return d
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app_n.run()
 
 
