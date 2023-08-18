d14906e0875af8aa026742a1e869edf86a6de0f1
MaryamMaksour <118085088+MaryamMaksour@users.noreply.github.com>
2023-08-18 21:45:38 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,19 +1,53 @@
 from flask import Flask, request, jsonify
-from app import ask
 from flask_ngrok import run_with_ngrok
 
-app_n = Flask(__name__)
-run_with_ngrok(app_n)
+from app import ask
+from Summarizing import summary
+from translate import translate
+from Assistant import ask_assistant
 
-@app_n.route('/', methods=['GET'])
-def api():
+api = Flask(__name__)
+run_with_ngrok(api)
+
+@api.route('/',methods=['GET', 'POST'])
+def chat():
     d = {}
     data = str(request.args['message'])
     answer = str(ask(data))
     d['answer'] = answer
     return d
 
+
+@api.route('/summary', methods=['GET', 'POST'])
+def summar():
+        d = {}
+        file = str(request.args['file'])
+        answer = ""
+        answer = str(summary(file))
+        d['answer'] = answer
+        return d
+    
+    
+    
+@api.route('/translatePDF',  methods=['GET', 'POST'])
+def trans_PDF():
+    d = {}
+    file = str(request.args['file'])
+    answer = str(translate(file))
+    d['answer'] = answer
+    return d
+
+
+
+@api.route('/assistant', methods=['GET', 'POST'])
+def assistant():
+    d = {}
+    file = str(request.args['message'])
+    answer = str(ask_assistant(file))
+    d['answer'] = answer
+    return d
+
 if __name__ == '__main__':
-    app_n.run()
+    api.run()
 
 
